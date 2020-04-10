d4dbcfc91a3d2f94fd38f98998637c3398268d85
LFS Driverless <gustaf-96@hotmail.com>
2020-04-10 16:09:49 +0200

    m

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from player import Player
 
+def valid_guess(guess, prev_guess):
+	if len(prev_guess) == 0:
+		return True
+	prev_guess = prev_guess[-1]
+	if guess is None:
+		return True
+	if guess[0] > prev_guess[0]:
+		return True
+	if guess[0] == prev_guess[0] and guess[1] > prev_guess[1]:
+		return True
+
+	return False
+	
+
 def round(players):
 	nplayers = len(players)
 	pindex = 0
 	prev_guess = []
 	guess = (0,0)
-
+	
 	player_dice = [0]*nplayers
 
 	for i in range(nplayers):
@@ -14,27 +28,33 @@ def round(players):
 
 	while guess is not None:
 		player = players[pindex]	
+		
 		guess = player.guess(pindex, prev_guess, nplayers, player_dice)
+		assert valid_guess(guess, prev_guess), 'Invalid guess'
+		print(guess)
+			
 		prev_guess.append(guess)
 		pindex = (pindex + 1) % nplayers
 	
 		
+	winner_index = 1	
 	if len(prev_guess) > 1:	
-		guess = prev_guess[len(prev_guess) - 2]
+		guess = prev_guess[-2]
 		truth = 0
 		for player in players:
 			truth += player.dice.count(guess[1])
-	
-		winner_index = (pindex - 2) % nplayers if guess[0] >= truth else (pindex - 1) % nplayers
-	else: #first player said bluff
-		winner_index = 1	
+		print('truth',truth,'guess',guess[0])
+		if guess[0] >= truth:
+			winner_index = (pindex - 2) % nplayers 
+		else:
+			winner_index = (pindex - 1) % nplayers
 
 	ret_players = players[winner_index:]
 	ret_players.extend(players[:winner_index]) 
 	
-	ret_players[nplayers - 1].ndice -= 1	
-	if ret_players[nplayers - 1].ndice == 0:
-		del ret_players[nplayers - 1] 
+	ret_players[-1].ndice -= 1	
+	if ret_players[-1].ndice == 0:
+		del ret_players[-1] 
 	
 	return ret_players
 
--- player.py
+++ player.py
@@ -4,20 +4,31 @@ class Player:
 
 	def __init__(self, ndice):
 		self.ndice = ndice
-		self.dice = [random.randint(1,7) for i in range(self.ndice)]
+		self.dice = [random.randint(1,6) for i in range(self.ndice)]
 	
 	def cast(self):
-		self.dice = [random.randint(1,7) for i in range(self.ndice)]
+		self.dice = [random.randint(1,6) for i in range(self.ndice)]
 	
 	#(Number of dice, value of die), None if bluff
-	def guess(self, player_index, previous_guesses, nplayers, players_dice):
+	def guess(self, player_index, previous_guesses, nplayers, players_ndice):
+		other_dice = 0
+		for nd in players_ndice:
+			other_dice += nd
+
+		other_dice -= self.ndice;
+		
 		if len(previous_guesses) > 0:
-			prev_guess = previous_guesses[len(previous_guesses) - 1]
-
-
-
-		if random.random() < 0.3:
-			return None
-		else:	
-			return (random.randint(1,7),random.randint(1,7))
-
+			guess = previous_guesses[len(previous_guesses) - 1]
+			if (1.0/6.0)*other_dice + self.dice.count(guess[1]) < guess[0]:
+				return None
+			else:
+				return (guess[0] + 1, guess[1])  
+		
+		n_of_each = [0]*6
+		for d in self.dice:
+			n_of_each[d - 1] += 1
+		
+		fmax = max(n_of_each)
+		most_of = n_of_each.index(fmax) + 1
+		n_of_that = int(1.0/6.0*other_dice) + fmax
+		return (n_of_that, most_of)
