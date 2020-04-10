6c04ffee529b90311816aaa9d6188242301a75f4
LFS Driverless <gustaf-96@hotmail.com>
2020-04-10 17:33:14 +0200

    m

--- main.py
+++ main.py
@@ -30,33 +30,36 @@ def round(players):
 		player = players[pindex]	
 		
 		guess = player.guess(pindex, prev_guess, nplayers, player_dice)
-		assert valid_guess(guess, prev_guess), 'Invalid guess'
-		print(guess)
+		if not valid_guess(guess, prev_guess):
+			break
 			
 		prev_guess.append(guess)
 		pindex = (pindex + 1) % nplayers
+	else:	
+		winner_index = 1	
+		if len(prev_guess) > 1:	
+			guess = prev_guess[-2]
+			truth = 0
+			for player in players:
+				truth += player.dice.count(guess[1])
+			print('truth',truth,'guess',guess[0])
+			if guess[0] >= truth:
+				winner_index = (pindex - 2) % nplayers 
+			else:
+				winner_index = (pindex - 1) % nplayers
 	
+		ret_players = players[winner_index:]
+		ret_players.extend(players[:winner_index]) 
 		
-	winner_index = 1	
-	if len(prev_guess) > 1:	
-		guess = prev_guess[-2]
-		truth = 0
-		for player in players:
-			truth += player.dice.count(guess[1])
-		print('truth',truth,'guess',guess[0])
-		if guess[0] >= truth:
-			winner_index = (pindex - 2) % nplayers 
-		else:
-			winner_index = (pindex - 1) % nplayers
-
-	ret_players = players[winner_index:]
-	ret_players.extend(players[:winner_index]) 
+		ret_players[-1].ndice -= 1	
+		if ret_players[-1].ndice == 0:
+			del ret_players[-1] 
+		
+		return ret_players
 	
-	ret_players[-1].ndice -= 1	
-	if ret_players[-1].ndice == 0:
-		del ret_players[-1] 
-	
-	return ret_players
+	#pindex made invalid guess
+	del players[pindex]
+	return players
 
 if __name__ == "__main__":
 	nplayers = 5
